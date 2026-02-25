a61ea3ceceadb913c41651a6329adf9eeb8dc687
EricaLeeJoowon <leejoowon03@gmail.com>
2026-02-25 21:07:15 +0900

    feat: initial spring boot setup

---
+++ src/main/java/me/rentsignal/RentsignalBackendApplication.java
@@ -0,0 +1,11 @@
+package me.rentsignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentsignalBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RentsignalBackendApplication.class, args);
+    }
+}
