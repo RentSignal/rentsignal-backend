90ba47aa11cccd3829e80d1513694435722e3528
ruddmslee <dlruddms326@naver.com>
2026-04-04 17:29:02 +0900

    Feat: Region 저장 구현

--- src/main/java/me/rentsignal/data/controller/DataController.java
+++ src/main/java/me/rentsignal/data/controller/DataController.java
@@ -1,6 +1,7 @@
 package me.rentsignal.data.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.rentsignal.data.service.DataService;
 import me.rentsignal.data.service.LegalDongImportService;
 import me.rentsignal.global.response.BaseResponse;
 import me.rentsignal.global.security.CustomPrincipal;
@@ -16,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataController {
 
     private final LegalDongImportService legalDongImportService;
+    private final DataService dataService;
 
     @PostMapping("/legal-dong")
     public ResponseEntity<?> saveLegalDong(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
         legalDongImportService.importLegalDongCsv(customPrincipal.getId());
+        return ResponseEntity.ok().body(BaseResponse.success(null));
+    }
+
+    @PostMapping("/region")
+    public ResponseEntity<?> saveRegion(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
+        dataService.saveRegion(customPrincipal.getId());
         return ResponseEntity.ok().body(BaseResponse.success(null));
     }
 
