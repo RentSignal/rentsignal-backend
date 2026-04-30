8f2bf19897e3d243c1c2246e6a2c12778cf44b35
ruddmslee <dlruddms326@naver.com>
2026-05-01 00:49:44 +0900

    Feat: 지하철역 데이터 저장

--- src/main/java/me/rentsignal/data/reader/SubwayCsvReader.java
+++ src/main/java/me/rentsignal/data/reader/SubwayCsvReader.java
@@ -3,7 +3,7 @@ package me.rentsignal.data.reader;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import lombok.extern.slf4j.Slf4j;
-import me.rentsignal.data.dto.BusStopCsvRowDto;
+import me.rentsignal.data.dto.SubwayCsvRowDto;
 import me.rentsignal.global.exception.BaseException;
 import me.rentsignal.global.exception.ErrorCode;
 import org.springframework.core.io.ClassPathResource;
@@ -17,42 +17,36 @@ import java.util.List;
 
 @Component
 @Slf4j
-public class BusStopCsvReader {
+public class SubwayCsvReader {
 
-    /** csv 파일의 행 -> BusStopCsvRowDto 변환 */
-    public List<BusStopCsvRowDto> read() {
-        List<BusStopCsvRowDto> rows = new ArrayList<>();
+    /** csv 파일의 행 -> SubwayCsvRowDto 변환 */
+    public List<SubwayCsvRowDto> read() {
+        List<SubwayCsvRowDto> rows = new ArrayList<>();
 
         try (
                 InputStreamReader isr = new InputStreamReader(
-                        new ClassPathResource("bus-stop.csv").getInputStream(), StandardCharsets.UTF_8
+                        new ClassPathResource("subway.csv").getInputStream(), StandardCharsets.UTF_8
                 );
 
-                // 정류장명에 쉼표가 들어가는 행이 있어 쉼표로 구분 X -> OpenCSV 사용
                 CSVReader reader = new CSVReader(isr)
         ){
-            String[] line;
-            Boolean isFirst = true;
+            String[] lines;
+            boolean isFirst = true;
 
-            while ((line = reader.readNext()) != null) {
+            while ((lines = reader.readNext()) != null) {
                 // csv 파일 맨 윗줄 헤더 스킵
                 if (isFirst) {
                     isFirst = false;
                     continue;
                 }
 
-                // 서울 데이터만 저장 시
-//                String province = line[7] == null ? "" : line[7].trim();
-//                if (!province.equals("서울특별시")) {
-//                    continue;
-//                }
-
-                String name = line[1];
-                String latitudeText = line[2] == null ? "" : line[2].trim();
-                String longitudeText = line[3] == null ? "" : line[3].trim();
+                String name = lines[1];
+                String line = lines[2];
+                String latitudeText = lines[3] == null ? "" : lines[3].trim();
+                String longitudeText = lines[4] == null ? "" : lines[4].trim();
 
                 if (latitudeText.isBlank() || longitudeText.isBlank()) {
-                    log.warn("위도/경도 누락 - name={} lat={}, lng={}", line[1], line[2], line[3]);
+                    log.warn("위도/경도 누락 - name={} lat={}, lng={}", lines[1], lines[3], lines[4]);
                     continue;
                 }
 
@@ -60,14 +54,15 @@ public class BusStopCsvReader {
                 Double longitude = Double.parseDouble(longitudeText);
 
                 rows.add(
-                        BusStopCsvRowDto.builder()
+                        SubwayCsvRowDto.builder()
                                 .name(name)
+                                .line(line)
                                 .latitude(latitude)
                                 .longitude(longitude).build());
             }
         } catch (IOException | CsvValidationException e) {
             log.error("데이터 로드 실패" + e.getMessage());
-            throw new BaseException(ErrorCode.CSV_READ_ERROR, "전국 버스정류장 데이터 csv 파일 읽기에 실패했습니다. - " + e.getMessage());
+            throw new BaseException(ErrorCode.CSV_READ_ERROR, "지하철 데이터 csv 파일 읽기에 실패했습니다. - " + e.getMessage());
         }
 
         return rows;
