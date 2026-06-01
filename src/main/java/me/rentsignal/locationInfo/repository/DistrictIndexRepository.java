e086890fd6b134f30ea8ce9d53f5acac651c9481
ruddmslee <dlruddms326@naver.com>
2026-06-02 02:39:09 +0900

    Feat: 가장 최신 baseYearMonth 기준으로 데이터 조회

--- src/main/java/me/rentsignal/locationInfo/repository/DistrictIndexRepository.java
+++ src/main/java/me/rentsignal/locationInfo/repository/DistrictIndexRepository.java
@@ -2,6 +2,7 @@ package me.rentsignal.locationInfo.repository;
 
 import me.rentsignal.locationInfo.entity.DistrictIndex;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,9 @@ import java.util.Optional;
 public interface DistrictIndexRepository extends JpaRepository<DistrictIndex, Long> {
     List<DistrictIndex> findByDistrict_Province_NameAndBaseYearMonthOrderBySubwayAccessibilityIndexDesc(String provinceName, String baseYearMonth);
     Optional<DistrictIndex> findByDistrict_IdAndBaseYearMonth(Long districtId, String baseYearMonth);
+    @Query("""
+    SELECT MAX(d.baseYearMonth)
+    FROM DistrictIndex d
+""")
+    String findLatestBaseYearMonth();
 }
--- src/main/java/me/rentsignal/locationInfo/repository/ProvinceIndexRepository.java
+++ src/main/java/me/rentsignal/locationInfo/repository/ProvinceIndexRepository.java
@@ -2,6 +2,7 @@ package me.rentsignal.locationInfo.repository;
 
 import me.rentsignal.locationInfo.entity.ProvinceIndex;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,9 @@ import java.util.Optional;
 public interface ProvinceIndexRepository extends JpaRepository<ProvinceIndex, Long> {
     Optional<ProvinceIndex> findByProvince_NameAndBaseYearMonth(String name, String baseYearMonth);
     List<ProvinceIndex> findByProvince_NameAndBaseYearMonthBetweenOrderByBaseYearMonthAsc(String name, String start, String end);
+    @Query("""
+    SELECT MAX(p.baseYearMonth)
+    FROM ProvinceIndex p
+""")
+    String findLatestBaseYearMonth();
 }
